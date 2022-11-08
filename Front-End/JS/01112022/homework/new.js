007c76342899a11bacf928479e64a4c00409f665
Volodymyr Ziubrytskyi <tarantul294@gmail.com>
2022-11-08 12:39:10 +0100

    late homework

--- Front-End/JS/01112022/homework/new.js
+++ Front-End/JS/01112022/homework/new.js
@@ -9,16 +9,34 @@ function _getAddress()
         let _assets = response.assets
         for (let i = 0; i < _assets.length; i++)
         {
-            console.log(i, "address:", _assets[i].creator.address);
+            console.log("address:", _assets[i].creator.address);
+            localStorage.setItem(i,_assets[i].creator.address);
         }
+        _getBalance();
     });
-    // _getBalance();
+ 
 }
 _getAddress();
 function _getBalance()
 {
-    fetch("https://api.ethplorer.io/getAddressInfo/0x495f947276749ce646f68ac8c248420045cb7b5e?apiKey=freekey",
+    for (let i = 0; i < localStorage.length; i++)
+    {
+        fetch("https://api.ethplorer.io/getAddressInfo/" + localStorage.getItem(i) + "?apiKey=freekey",
     {
         method: 'GET'
-    }).then(response => response.json()).then((response) => console.log(response))
+    }).then(response => response.json()).then((response) => 
+        {
+            console.log("balance: ", response.ETH.balance)
+            sessionStorage.setItem(i,response.ETH.balance);
+            _getdata();
+        })
+    }
+}
+function _getdata()
+{
+    for (let i = 0; i < localStorage.length; i++)
+    {
+        console.log(JSON.parse(localStorage.getItem(i), sessionStorage.getItem(i)));
+    }
+    
 }
