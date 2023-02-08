9cdbd66d12ea966b931d6bd4858dde268c99a763
Volodymyr Ziubrytskyi <tarantul294@gmail.com>
2023-02-08 14:06:08 +0100

    homework with buhone

--- Front-End/REACT/buhone/src/Components/Nav/Nav.jsx
+++ Front-End/REACT/buhone/src/Components/Nav/Nav.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+// eslint-disable-next-line no-unused-vars
 import nav from "./nav.scss";
 
 export default function Nav() {
--- Front-End/REACT/buhone/src/Components/Offers/Offers.jsx
+++ Front-End/REACT/buhone/src/Components/Offers/Offers.jsx
@@ -1,9 +1,46 @@
 import React, { useState } from "react";
+// eslint-disable-next-line no-unused-vars
+import offers from "./offers.scss"
 
 export default function Offers() {
+    let offersItems = [
+        {
+            title: 'Бухгалтерское обслуживание',
+            buttonText: 'Подробнее'
+        },
+        {
+            title: 'Бухгалтерское обслуживание',
+            buttonText: 'Подробнее'
+        },
+        {
+            title: 'Бухгалтерское обслуживание',
+            buttonText: 'Подробнее'
+        },
+        {
+            title: 'Бухгалтерское обслуживание',
+            buttonText: 'Подробнее'
+        },
+        {
+            title: 'Бухгалтерское обслуживание',
+            buttonText: 'Подробнее'
+        },
+        {
+            title: 'Бухгалтерское обслуживание',
+            buttonText: 'Подробнее'
+        },
+    ]
     return(
-        <section>
-            
+        <section className="offers">
+            <p className="offers__subtitle">Наши услуги</p>
+            <h3 className="offers__title">Мы специализируемся</h3>
+            <div className="offers__description">
+                {offersItems.map(item => (
+                    <div className="description__block">
+                        <h4 className="block__title">{item.title}</h4>
+                        <button className="block__button">{item.buttonText}</button>
+                    </div>
+                ))}
+            </div>
         </section>
     );
 }
